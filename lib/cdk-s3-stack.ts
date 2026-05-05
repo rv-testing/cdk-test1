969f79e6d1eb1e6ba27ccabe00fc1306d2e6b077
venerari <venerayan@gmail.com>
2026-05-04 23:02:21 -0400

    update

--- lib/cdk-s3-stack.ts
+++ lib/cdk-s3-stack.ts
@@ -22,3 +22,4 @@ export class CdkS3Stack extends cdk.Stack {
     });
   }
 }
+# lib/cdk-s3-stack.ts defines a CDK stack for creating an S3 bucket. The bucket is configured with versioning, public access blocking, S3-managed encryption, and SSL enforcement. Additionally, the bucket is set to be destroyed along with its objects when the stack is deleted. An output is also defined to display the name of the created bucket.
